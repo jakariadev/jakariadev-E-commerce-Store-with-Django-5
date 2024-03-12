84c18f452175195b693dd40cc7280b76a1bc609c
Md. Jakaria <jakaria.pust@gmail.com>
2024-03-12 12:11:45 +0600

    Cart update views.py updated for product id Issue. Also requirements.txt added

--- cart/views.py
+++ cart/views.py
@@ -28,14 +28,13 @@ def cart_add(request):
 
 
 def cart_update(request):
-    print("REQUEST_______:", request)
     cart = Cart(request)
 
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('product_id'))
         product_quantity = int(request.POST.get('product_quantity'))
 
-        cart.update(product=product_id, qty=product_quantity)
+        cart.update(product_id, product_quantity)
 
         cart_qty = cart.__len__()
         cart_total = cart.get_total()
